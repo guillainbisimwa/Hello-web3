2cb55c8f475fe2e168298a9cd47883f235d108e3
guillainbisimwa <guillainbisimwa@hotmail.fr>
2022-09-23 19:58:15 +0200

    Interact with my first contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,17 +1,21 @@
 /**
      * @type import('hardhat/config').HardhatUserConfig
      */
+
  require('dotenv').config(); //all the key value pairs are being made available due to this lib
+ 
  require('@nomiclabs/hardhat-ethers');
+ 
  const {API_URL_KEY, PRIVATE_KEY} = process.env; //environment variables are being loaded here.
+ 
  module.exports = {
    solidity: "0.8.17",
    defaultNetwork: 'goerli',
    networks: {
      hardhat: {},
      goerli: {
-         url: API_URL_KEY,
-         accounts: [`0x${PRIVATE_KEY}`]
+         url: API_URL_KEY, //alchemy
+         accounts: [`0x${PRIVATE_KEY}`] //private key from MM
      }
    }
  };
